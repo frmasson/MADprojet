51b5224eac2d0240cc88a6f5259b08a4c8ab7b7b
Cmillot <millot.christophe@courrier.uqam.ca>
2013-12-05 11:22:46 -0500

    modification sur Player et game

--- src/mad/game/Game.java
+++ src/mad/game/Game.java
@@ -1,33 +1,56 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mad.game;
 
 import java.util.ArrayList;
-import java.util.TreeSet;
 import mad.cards.Card;
+import mad.cards.CardSet;
 
 /**
  *
  * @author Christophe
  */
 public class Game {
+
     private ArrayList players;
-    private TreeSet cards;
-    private TreeSet discardedCards;
-    
-    
-    public void addPlayer(Player player){
+    private CardSet cards;
+    private CardSet discardedCards;
+    private int startingHitPoints;
+    private int nbTours = 0;
+
+    public Game(int nbPlayers, int startingHitPoints, CardSet cards) {
+        this.cards = cards;
+        this.cards.shuffle();
+        this.startingHitPoints = startingHitPoints;
+        createPlayers(nbPlayers);
+    }
+
+    private void createPlayers(int nbPlayers) {
+        Player newPlayer = new HumanPlayer(startingHitPoints);
+        createStartingPlayerCardSet(newPlayer);
+        addPlayer(newPlayer);
+        for (int i = 0; i <= nbPlayers; i++) {
+            newPlayer = new AIPlayer(startingHitPoints);
+            createStartingPlayerCardSet(newPlayer);
+            addPlayer(newPlayer);
+        }
+    }
+
+    private void createStartingPlayerCardSet(Player player) {
+        boolean isntFull = true;
+        while (isntFull) {
+            isntFull = player.addCard(cards.pop());
+        }
+    }
+
+    public void addPlayer(Player player) {
         players.add(player);
     }
-    
-    public void removePlayer(Player player){
+
+    public void removePlayer(Player player) {
         players.remove(player);
     }
-    
-    public void killPlayer(Player player){
-    //@TODO
+
+    public void killPlayer(Player player) {
+        //@TODO
     }
 
     public ArrayList getPlayers() {
@@ -38,23 +61,36 @@ public class Game {
         this.players = Players;
     }
 
-    public TreeSet getCards() {
+    public CardSet getCards() {
         return cards;
     }
 
-    public void setCards(TreeSet cards) {
+    public void setCards(CardSet cards) {
         this.cards = cards;
     }
 
-    public TreeSet getDiscardedCards() {
+    public CardSet getDiscardedCards() {
         return discardedCards;
     }
 
-    public void setDiscardedCards(TreeSet discardedCards) {
+    public void setDiscardedCards(CardSet discardedCards) {
         this.discardedCards = discardedCards;
     }
-    
+
     public void addDiscardedCard(Card card) {
         this.discardedCards.add(card);
     }
+
+    public int getNbTours() {
+        return nbTours;
+    }
+
+    public void incrementNbTours() {
+        ++this.nbTours;
+    }
+
+    public void playRound(){
+    
+    }
 }
+
